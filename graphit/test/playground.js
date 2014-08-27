bfc1e475298b09423506b637be31d579979db60f
showi <joachim.basmaison@gmail.com>
2014-08-27 23:36:58 +0200

    265 square ...

--- graphit/test/playground.js
+++ graphit/test/playground.js
@@ -4,43 +4,54 @@ define(function(require) {
 
     var Layer = require('../draw/Layer');
     var Canvas = require('graphit/draw/Canvas');
-    
+
     var Mixin = {
-            mixit: function(value) {
-                return "<mixin>" + value + "</mixin>";
-            }
+        mixit : function(value) {
+            return "<mixin>" + value + "</mixin>";
+        }
     };
 
-    function Mixin2 () {
-        
+    function Mixin2() {
+
     }
     Mixin2.prototype.mixit2 = function(value) {
-      return '<mixin2>'+value+'</mixin2>';  
+        return '<mixin2>' + value + '</mixin2>';
     };
     function MODULE() {
         this.__MODULE__ = 'graphit/test/playground';
     }
 
     MODULE.prototype.run = function() {
-       api.log.title(this.__MODULE__);
+        api.log.title(this.__MODULE__);
 
-       
-       var Canvas = graphit.factory.Canvas;
-       console.log('Canvas', Canvas);
-       api.util.injectMixin(Canvas, Mixin);
-       api.util.injectMixin(Canvas, Mixin2);
+        var Canvas = graphit.factory.Canvas;
+        console.log('Canvas', Canvas);
+        api.util.injectMixin(Canvas, Mixin);
+        api.util.injectMixin(Canvas, Mixin2);
 
-       var opts = { width: 200, height: 200 };
-       var canvas = new Canvas(opts);
-       jQuery('body').append(canvas.getElement());
-       var ctx = canvas.getCtx();
-       ctx.fillStyle = 'red';
-       api.shape.rectangle(ctx, 0, 0, opts.width, opts.height);
-       ctx.fillStyle = 'white';
-       api.shape.rectangle(ctx, 10, 10, opts.width - 20, opts.height - 20);
-       ctx.fillStyle = 'red';
-       api.shape.rectangle(ctx, 20, 20, opts.width - 40, opts.height - 40);
-       
+        var opts = {
+            width : 50,
+            height : 50
+        };
+        var step = 1;
+
+        for (var i = 0; i < 256; i++) {
+            var canvas = new Canvas(opts);
+            jQuery('body').append(canvas.getElement());
+            var ctx = canvas.getCtx();
+
+            function stepRectangle(step) {
+                api.shape.rectangle(ctx, step, step, opts.width - (step * 2),
+                        opts.height - (step * 2));
+            }
+            ctx.fillStyle = 'red';
+            api.shape.rectangle(ctx, 0, 0, opts.width, opts.height);
+            ctx.fillStyle = 'white';
+            stepRectangle(step * 2);
+            ctx.fillStyle = 'red';
+            stepRectangle(step * 4);
+            jQuery(canvas.getElement()).draggable();
+        }
     };
     return new MODULE();
 });
