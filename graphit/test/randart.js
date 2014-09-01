d77a8c1865fdab6b01b0bc73209ca2be3528af89
showi <joachim.basmaison@gmail.com>
2014-09-01 23:44:39 +0200

    Using document size

--- graphit/test/randart.js
+++ graphit/test/randart.js
@@ -3,12 +3,17 @@ define(function(require) {
 
     var Canvas = require('graphit/draw/Canvas');
     var shape = require('graphit/draw/shape');
-    var util = require('graphit/draw/tool');
+    var tool = require('graphit/draw/tool');
+    var util = require('graphit/util');
 
     function RANDART() {
         this.__MODULE__ = 'graphit/test/randart';
-        this.width = 640;
-        this.height = 480;
+        var size = util.getDocumentSize();
+        this.size = size;
+        this.ratio = 2.0;
+        this.width = Math.round(size[0]  / this.ratio);
+        this.height = Math.round(size[1] / this.ratio);
+        console.log('w/h', this.width, this.height);
     }
     RANDART.prototype.run = function() {
         var body = jQuery('body');
@@ -22,6 +27,8 @@ define(function(require) {
             top : '1em',
             left : '1em'
         });
+        jQuery(canvas.getElement()).css({ align:'center',
+                                            'vertical-align': 'middle'});
         body.append(canvas.getElement());
         var that = this;
         var max = 128;
@@ -29,37 +36,55 @@ define(function(require) {
         this.max = this.width;
         var rotate = 0;
         var miniature = [];
+        function removeMiniature(canvas) {
+            
+        }
+        var ratio = 1.0 / this.ratio;
+        var mWidth = this.width / this.ratio;
+        var mHeight = this.height / this.ratio;
+        console.log('mW/mH', mWidth, mHeight);
+        var row = Math.round(this.size[0] / mWidth);
+        var col = Math.round(this.size[1] / mHeight);
+        console.log('row/col', row, col);
+        var maxMiniature = row * col;
+        console.log('MaxMiniature', maxMiniature);
         function fn() {
-            if (miniature.length > 16) {
-                var c = miniature.pop();
-                jQuery('#' + c.getElement().id).remove();
-                doClean = 0;
-            }
-            if (count > max) {
-                var n = canvas.downScale(0.5);
-                miniature.unshift(n);
-                body.append(n.getElement());
-                count = 0;
-                ctx.fillStyle = util.randomColor();
-                shape.rectangle(ctx, 0, 0, that.width, that.height);
-                if (randomBool(0.5)) {
-                    ctx.fillStyle = util.randomColor();
+            for (var i = 0; i < 10; i++) {
+                if (miniature.length > maxMiniature) {
+                    var c = miniature.pop();
+                    function cb() {
+                        c.getElement().remove();
+                    }
+                    setTimeout(cb, 1);
                 }
+                if (count > max) {
+                    var scale = 1.0 / that.ratio;
+                    console.log('scale', scale);
+                    var n = canvas.downScale(scale);
+                    miniature.unshift(n);
+                    body.append(n.getElement());
+                    count = 0;
+                    ctx.fillStyle = tool.randomColor();
+                    shape.rectangle(ctx, 0, 0, that.width, that.height);
+                    if (randomBool(0.5)) {
+                        ctx.fillStyle = tool.randomColor();
+                    }
+                }
+                rotate = (rotate > 360)? 0: rotate + 1;
+                count++;
+                ctx.rotate(rotate);
+                ctx.lineWidth = randInt(5);
+                if (randomBool(0.8)) {
+                    ctx.fillStyle = tool.randomColor();
+                }
+                if (randomBool(0.8)) {
+                    ctx.fillStyle = tool.randomColor();
+                }
+                var kind = choice(['line', 'rectangle', 'circle']);
+                var method = 'draw_' + kind;
+                that[method].call(that, ctx);
             }
-            rotate = (rotate > 360)? 0: rotate + 1;
-            count++;
-            ctx.rotate(rotate);
-            ctx.lineWidth = randInt(5);
-            if (randomBool(0.8)) {
-                ctx.fillStyle = util.randomColor();
-            }
-            if (randomBool(0.8)) {
-                ctx.fillStyle = util.randomColor();
-            }
-            var kind = choice(['line', 'rectangle', 'circle']);
-            var method = 'draw_' + kind;
-            that[method].call(that, ctx);
-            setTimeout(fn, 10);
+            setTimeout(fn, 100);
         };
         fn();
     };
