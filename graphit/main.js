b39ab68e444f46b3fdc282438c413473274821b4
showi <joachim.basmaison@gmail.com>
2014-09-02 11:20:38 +0200

    movingpaint..

--- graphit/main.js
+++ graphit/main.js
@@ -2,10 +2,11 @@ define(function(require) {
     var util = require('graphit/util');
     var choices = ['randart', 'clock', 'wall', 'movingpaint'];
     var choice = util.choice(choices);
+    choice = 'movingpaint';
     console.log('>>> random test:', choice, '(', choices, ')');
     util.runTest(choice);
-    function reload() {
-            window.location.reload();
-    }
-    setTimeout(reload, 1000*60);
+//    function reload() {
+//            window.location.reload();
+//    }
+//    setTimeout(reload, 1000*60);
 });
